f09276430bd6c4402fced5e8ddb976378816b08b
Emilio Sala <emilio_k88@hotmail.com>
2026-05-08 10:27:26 +0200

    ADD: enum for keeping the state in Request struct

--- internal/request/request.go
+++ internal/request/request.go
@@ -8,6 +8,7 @@ import (
 
 type Request struct {
 	RequestLine RequestLine
+	state       parserState
 	// Headers     map[string]string
 	// Body        []byte
 }
@@ -17,6 +18,13 @@ type RequestLine struct {
 	RequestTarget string
 	Method        string
 }
+
+type parserState int
+
+const (
+	stateInitialized parserState = iota //0
+	stateDone                           //1
+)
 
 const crlf = "\r\n"
 
@@ -79,3 +87,65 @@ func newRequestLine(line string) (*RequestLine, error) {
 		HttpVersion:   version,
 	}, nil
 }
+
+/*
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	requestLine, err := parseRequestLine(data)
+	if err != nil {
+		return nil, err
+	}
+	return &Request{
+		RequestLine: *requestLine,
+	}, nil
+}
+
+func parseRequestLine(data []byte) (*RequestLine, error) {
+	dataStr := string(data)
+	idx := strings.Index(dataStr, crlf)
+	if idx == -1 {
+		return nil, fmt.Errorf("CRLF not found")
+	}
+	return newRequestLine(dataStr[:idx])
+}
+
+func newRequestLine(line string) (*RequestLine, error) {
+	parts := strings.Split(line, " ")
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("Expect: 3 parts, got: %d", len(parts))
+	}
+	method := parts[0]
+	for _, c := range method {
+		if c < 'A' || c > 'Z' {
+			return nil, fmt.Errorf("invalid method: %s", method)
+		}
+	}
+	target := parts[1]
+	if parts[2] != "HTTP/1.1" {
+		return nil, fmt.Errorf("Expect: HTTP/1.1, got: %s", parts[2])
+	}
+
+	versionParts := strings.Split(parts[2], "/")
+	if len(versionParts) != 2 {
+		return nil, fmt.Errorf("invalid version: %s", versionParts)
+	}
+
+	httpPart := versionParts[0]
+	if httpPart != "HTTP" {
+		return nil, fmt.Errorf("unrecognized HTTP-version: %s", httpPart)
+	}
+	version := versionParts[1]
+	if version != "1.1" {
+		return nil, fmt.Errorf("unrecognized HTTP-version: %s", version)
+	}
+
+	return &RequestLine{
+		Method:        method,
+		RequestTarget: target,
+		HttpVersion:   version,
+	}, nil
+}
+*/
